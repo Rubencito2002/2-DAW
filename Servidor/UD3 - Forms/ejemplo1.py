5a515ecfc38a0a713ea2b2de98d2161df2c0ea57
Rubencito2002 <ruben.machado1920@gmail.com>
2023-11-10 11:26:59 +0100

    update servidor

--- Servidor/UD3 - Forms/ejemplo1.py
+++ Servidor/UD3 - Forms/ejemplo1.py
@@ -4,7 +4,14 @@ from django.shortcuts import render
 from .forms import MyForm
 
 class myviews(views):
+    template_name = 'form_template.html'
+    def get(self,request):
+        form = MyForm(initial={"key": 'value'})
+        return render(request, self.template_name, {'form': form})
+
     def post(self, request):
         form = MyForm(request.POST)
         if form.is_valid():
+            # <process form cleaned data>
             return HttpResponseRedirect('/success/')
+        return render(request, self.template_name, {'form': form})
