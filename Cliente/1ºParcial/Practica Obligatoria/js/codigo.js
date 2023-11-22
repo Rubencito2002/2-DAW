f524560f93616647c87a42e27c824e9d3522aba7
Rubencito2002 <ruben.machado1920@gmail.com>
2023-11-22 14:49:40 +0100

    Refactor code and add new features

--- Cliente/1ºParcial/Practica Obligatoria/js/codigo.js
+++ Cliente/1ºParcial/Practica Obligatoria/js/codigo.js
@@ -1,28 +1,37 @@
 "use strict";
 // Variables globales
-let oVivero = new Vivero();
+let oAgencia = new Agencia();
 
 datosIniciales();
 
 function datosIniciales() {
-  oVivero.altaArbol(new Perenne(1, 100, "Olivo", true));
-  oVivero.altaArbol(new Caduco(2, 78, "Melocotonero", "abril"));
-  oVivero.altaArbol(new Perenne(3, 50, "Ciprés", false));
-  oVivero.altaArbol(new Perenne(4, 75, "Pino piñonero", true));
-  oVivero.altaArbol(new Caduco(5, 81, "Melocotonero", "abril"));
-  oVivero.altaArbol(new Caduco(6, 110, "Manzano", "mayo"));
-  oVivero.altaArbol(new Perenne(7, 80, "Cedro", false));
-  oVivero.altaArbol(new Caduco(8, 67, "Naranjo", "marzo"));
-  oVivero.altaArbol(new Perenne(9, 90, "Alcornoque", true));
-  oVivero.altaArbol(new Caduco(10, 70, "Peral", "marzo"));
+  oAgencia.altaCliente(new Cliente(11651, "Pepe", "Perez", "pepe51"));
+  oAgencia.altaCliente(new Cliente(17829, "Laura", "Pastrana", "laura29"));
+  oAgencia.altaCliente(new Cliente(19235, "Juan", "Machado", "juan35"));
+
+  oAgencia.altaVehiculo(new Vehiculo("ABC123", "Toyota", "Corolla","Gasolina", 5));
+  oAgencia.altaVehiculo(new Vehiculo("DEF789", "Yamaha", "YZF-R3", true));
+  oAgencia.altaVehiculo(new Vehiculo("XYZ456", "Honda", "Civic", "Gasolina", 4));
+  oAgencia.altaVehiculo(new Vehiculo("MTO567", "Honda", "CBR500R", false));
+
+  oAgencia.altaAlquiler(new Alquiler(1, "pepe51", "ABC123", "15/02/2023", "20/05/2023"));
+  oAgencia.altaAlquiler(new Alquiler(2, "laura29", "XYZ456", "01/06/2023", "15/08/2023"));
+  oAgencia.altaAlquiler(new Alquiler(3, "juan35", "MTO567", "10/09/2023", "31/12/2023"));
 }
 
 // Gestión de formularios
-function gestionFormularios(sFormularioVisible) {
+function gestionFormularios(sFormularioVisible) 
+{
   ocultarTodosLosFormularios();
 
   // Hacemos visible el formulario que llega como parámetro
   switch (sFormularioVisible) {
+    case "frmAltaCliente":
+      frmAltaCliente.style.display = "block";
+      break;
+    case "frmAltaVehiculo":
+      frmAltaVehiculo.style.display = "block";
+      break;
     case "frmAltaArbol":
       frmAltaArbol.style.display = "block";
       break;
@@ -36,82 +45,8 @@ function gestionFormularios(sFormularioVisible) {
       frmListadoCaducos.style.display = "block";
       break;
     case "TotalArboles":
-      alert("Hay " + oVivero.totalArbolesVenta() + " árboles a la venta");
+      alert("Hay " + oAgencia.totalArbolesVenta() + " árboles a la venta");
       break;
   }
 }
 
-function mostrarAltaArbol() {
-  ocultarTodosLosFormularios();
-
-  // Hacemos visible el formulario
-  frmAltaArbol.style.display = "block";
-}
-
-function ocultarTodosLosFormularios() {
-  let oFormularios = document.querySelectorAll("form");
-
-  for (let i = 0; i < oFormularios.length; i++) {
-    oFormularios[i].style.display = "none";
-  }
-}
-
-// aceptarAltaArbol
-function aceptarAltaArbol() {
-  // Insertar el nuevo árbol
-  if (oVivero.altaArbol(oArbol)) {
-    alert("Arbol registrado OK");
-    frmAltaArbol.reset(); // Vaciamos los campos del formulario
-    frmAltaArbol.style.display = "none";
-  } else {
-    alert("Arbol registrado previamente");
-  }
-}
-
-function aceptarTallaje() {
-  let sRespuesta =
-    /*Llamada a tallajeArbol*/
-
-    alert(sRespuesta);
-
-  if (sRespuesta.includes("Correcto") > 0) {
-    frmTallaje.reset();
-    frmTallaje.style.display = "none";
-  }
-}
-
-function aceptarListadoPerennes() {
-  //Crear el listado
-
-  let oVentana = open("", "_blank", "");
-
-  oVentana.document.open();
-  oVentana.document.write(
-    "<h1>Listado de árboles perennes de altura mínima: " + iAlturaMinima + " cm</h1>"
-  );
-  oVentana.document.write(/*Listado a mostrar*/);
-  oVentana.document.close();
-  oVentana.document.title = "Listado perennes";
-
-  // Reseteamos y ocultamos el formulario
-  frmListadoPerennes.reset();
-  frmListadoPerennes.style.display = "none";
-}
-
-function aceptarListadoCaducos() {
-  //Crear el listado
-
-  let oVentana = open("", "_blank", "");
-
-  oVentana.document.open();
-  oVentana.document.write(
-    "<h1>Listado de árboles caducos con floración el mes: " + sMesFloracion + "</h1>"
-  );
-  oVentana.document.write(/*listado a mostrar*/);
-  oVentana.document.close();
-  oVentana.document.title = "Listado caducos";
-
-  // Reseteamos y ocultamos el formulario
-  frmListadoCaducos.reset();
-  frmListadoCaducos.style.display = "none";
-}
