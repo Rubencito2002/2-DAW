04bf6696b5ae1d51a59875e49cacb316f4307215
Rubencito2002 <ruben.machado1920@gmail.com>
2023-11-24 14:53:27 +0100

    update practica (cliente)

--- Cliente/Bloque I/Practica Obligatoria/js/codigo.js
+++ Cliente/Bloque I/Practica Obligatoria/js/codigo.js
@@ -1,27 +1,24 @@
 "use strict";
 // Variables globales
-let oAgencia = new Agencia();
+let oVivero = new Vivero();
 
 datosIniciales();
 
 function datosIniciales() {
-  oAgencia.altaCliente(new Cliente(11651, "Pepe", "Perez", "pepe51"));
-  oAgencia.altaCliente(new Cliente(17829, "Laura", "Pastrana", "laura29"));
-  oAgencia.altaCliente(new Cliente(19235, "Juan", "Machado", "juan35"));
-
-  oAgencia.altaVehiculo(new Vehiculo("ABC123", "Toyota", "Corolla","Gasolina", 5));
-  oAgencia.altaVehiculo(new Vehiculo("DEF789", "Yamaha", "YZF-R3", true));
-  oAgencia.altaVehiculo(new Vehiculo("XYZ456", "Honda", "Civic", "Gasolina", 4));
-  oAgencia.altaVehiculo(new Vehiculo("MTO567", "Honda", "CBR500R", false));
-
-  oAgencia.altaAlquiler(new Alquiler(1, "pepe51", "ABC123", "15/02/2023", "20/05/2023"));
-  oAgencia.altaAlquiler(new Alquiler(2, "laura29", "XYZ456", "01/06/2023", "15/08/2023"));
-  oAgencia.altaAlquiler(new Alquiler(3, "juan35", "MTO567", "10/09/2023", "31/12/2023"));
+  oVivero.altaArbol(new Perenne(1, 100, "Olivo", true));
+  oVivero.altaArbol(new Caduco(2, 78, "Melocotonero", "abril"));
+  oVivero.altaArbol(new Perenne(3, 50, "Ciprés", false));
+  oVivero.altaArbol(new Perenne(4, 75, "Pino piñonero", true));
+  oVivero.altaArbol(new Caduco(5, 81, "Melocotonero", "abril"));
+  oVivero.altaArbol(new Caduco(6, 110, "Manzano", "mayo"));
+  oVivero.altaArbol(new Perenne(7, 80, "Cedro", false));
+  oVivero.altaArbol(new Caduco(8, 67, "Naranjo", "marzo"));
+  oVivero.altaArbol(new Perenne(9, 90, "Alcornoque", true));
+  oVivero.altaArbol(new Caduco(10, 70, "Peral", "marzo"));
 }
 
 // Gestión de formularios
-function gestionFormularios(sFormularioVisible) 
-{
+function gestionFormularios(sFormularioVisible) {
   ocultarTodosLosFormularios();
 
   // Hacemos visible el formulario que llega como parámetro
@@ -32,21 +29,101 @@ function gestionFormularios(sFormularioVisible)
     case "frmAltaVehiculo":
       frmAltaVehiculo.style.display = "block";
       break;
-    case "frmAltaArbol":
-      frmAltaArbol.style.display = "block";
+    case "frmAltaAlquiler":
+      frmAltaAlquiler.style.display = "block";
       break;
-    case "frmTallaje":
-      frmTallaje.style.display = "block";
+    case "frmBajaAlquiler":
+      frmBajaAlquiler.style.display = "block";
       break;
-    case "frmListadoPerennes":
-      frmListadoPerennes.style.display = "block";
+    case "frmListadoCliente":
+      frmListadoCliente.style.display = "block";
       break;
-    case "frmListadoCaducos":
-      frmListadoCaducos.style.display = "block";
+    case "frmListadoVehiculo":
+      frmListadoVehiculo.style.display = "block";
       break;
-    case "TotalArboles":
-      alert("Hay " + oAgencia.totalArbolesVenta() + " árboles a la venta");
+    case "frmListadoAlquilerPorFecha":
+      frmListadoAlquilerPorFecha.style.display = "block";
+      break;
+    case "frmListadoAlquilerPorCliente":
+      frmListadoAlquilerPorCliente.style.display = "block";
+      break;
+    case "frmListadoCochesElectrico":
+      frmListadoCochesElectrico.style.display = "block";
       break;
   }
 }
 
+function mostrarAltaArbol() {
+  ocultarTodosLosFormularios();
+
+  // Hacemos visible el formulario
+  frmAltaCliente.style.display = "block";
+}
+
+function ocultarTodosLosFormularios() {
+  let oFormularios = document.querySelectorAll("form");
+
+  for (let i = 0; i < oFormularios.length; i++) {
+    oFormularios[i].style.display = "none";
+  }
+}
+
+// AceptarAltaCliente
+function AceptarAltaCliente() {
+  // Insertar el nuevo cliente
+  if (oVivero.altaArbol(oArbol)) {
+    alert("Arbol registrado OK");
+    frmAltaCliente.reset(); // Vaciamos los campos del formulario
+    frmAltaCliente.style.display = "none";
+  } else {
+    alert("Arbol registrado previamente");
+  }
+}
+
+function aceptarAltaVehiculo() {
+  let sRespuesta =
+    /*Llamada a tallajeArbol*/
+
+    alert(sRespuesta);
+
+  if (sRespuesta.includes("Correcto") > 0) {
+    frmAltaVehiculo.reset();
+    frmAltaVehiculo.style.display = "none";
+  }
+}
+
+function aceptarListadoPerennes() {
+  //Crear el listado
+
+  let oVentana = open("", "_blank", "");
+
+  oVentana.document.open();
+  oVentana.document.write(
+    "<h1>Listado de árboles perennes de altura mínima: " + iAlturaMinima + " cm</h1>"
+  );
+  oVentana.document.write(/*Listado a mostrar*/);
+  oVentana.document.close();
+  oVentana.document.title = "Listado perennes";
+
+  // Reseteamos y ocultamos el formulario
+  frmListadoCliente.reset();
+  frmListadoCliente.style.display = "none";
+}
+
+function aceptarListadoCaducos() {
+  //Crear el listado
+
+  let oVentana = open("", "_blank", "");
+
+  oVentana.document.open();
+  oVentana.document.write(
+    "<h1>Listado de árboles caducos con floración el mes: " + sMesFloracion + "</h1>"
+  );
+  oVentana.document.write(/*listado a mostrar*/);
+  oVentana.document.close();
+  oVentana.document.title = "Listado caducos";
+
+  // Reseteamos y ocultamos el formulario
+  frmListadoCaducos.reset();
+  frmListadoCaducos.style.display = "none";
+}
