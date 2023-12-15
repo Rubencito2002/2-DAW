c92406a592214ed3a7cdd0535ea97cfc6809ae66
Rubencito2002 <ruben.machado1920@gmail.com>
2023-12-15 14:56:39 +0100

    update cliente

--- Cliente/Bloque II/UD5 - Interacción con el usuario. Eventos y formularios/Boletin Ejercicios/EJ01/ej01.js
+++ Cliente/Bloque II/UD5 - Interacción con el usuario. Eventos y formularios/Boletin Ejercicios/EJ01/ej01.js
@@ -1,1 +1,14 @@
 // Dado el formulario con los datos de tres actores seleccionables mediante un radiobutton, añadir un manejador de eventos al botón para que al pulsar sobre él muestre por consola los datos del actor seleccionado.
+formulario.consultar.addEventListener("click", mostrarDatos) 
+function mostrarDatos()
+{
+    for (let actor of formulario.actores) 
+        {
+            if (actor.checked) 
+            {
+                actorSeleccionado = radio.value;
+                break;
+            }
+        }
+}
+        
