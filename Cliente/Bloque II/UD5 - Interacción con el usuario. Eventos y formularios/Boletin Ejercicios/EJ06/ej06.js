e1b80e4eae702e475f56e5b1ac624c73de0ecdc3
rubencito2002 <ruben.machado1920@gmail.com>
2024-01-10 14:35:32 +0100

    ejercicio de cliente

--- Cliente/Bloque II/UD5 - Interacción con el usuario. Eventos y formularios/Boletin Ejercicios/EJ06/ej06.js
+++ Cliente/Bloque II/UD5 - Interacción con el usuario. Eventos y formularios/Boletin Ejercicios/EJ06/ej06.js
@@ -1,1 +1,4 @@
+// Dado el documento HTML facilitado con este ejercicio, añadir los manejadores de eventos necesarios para:
+// El cuadrado se ponga de color amarillo cuando el cursor del ratón se coloque encima del mismo, para ello usa el atributo classList añadiendo la clase amarillo preparada de antemano. Además deberá informar en la capa de salida de texto del tipo de evento, el objeto en el que se produce, así como las coordenadas del cursor en el momento que se desencadena. Cuando el cursor salga del cuadrado deberá volver todo a la situación original, cuadrado blanco y salida de texto vacía.
+// Cuando el cursor se coloque en el input de texto, cada vez que se pulse una tecla se informará en la capa de salida de texto de la tecla pulsada.
 
